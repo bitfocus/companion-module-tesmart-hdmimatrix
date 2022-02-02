aa06a1105190c6ccd75570bdc6d5fbb5216e3f3a
Julian Waller <git@julusian.co.uk>
2022-02-02 16:44:07 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ instance.prototype.destroy = function() {
 instance.prototype.actions = function(system) {
 	var self = this;
 
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 		'sendToAllOutputs': {
 			label: 'Send Input to All Outputs',
 			options: [
